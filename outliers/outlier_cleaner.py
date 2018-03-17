c8e7dc7f355bd6568cb70d6df395e3ee0b485489
anandvimal <colirsweb@gmail.com>
2018-03-17 12:05:06 -0500

    this commit is after outliers but somewhere in middle of culstering chapter. i have not started mini project yet. so pretty much at start of clustering.

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,26 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
+    from operator import itemgetter
+    errors = abs(net_worths - predictions)**2
+
+    data = zip(ages, net_worths, errors)
+    data.sort(key=itemgetter(2), reverse=True)
+    return data[9:]
+
+    '''
     ### your code goes here
+    #print("this is running !")
+    for p,a,n in zip(predictions, ages, net_worth):
+        e = abs(p-n)
+        t= (a,n,e)
+        cleaned_data.append(t)
+    '''
 
-    
-    return cleaned_data
-
+    #return cleaned_data[limit:]
