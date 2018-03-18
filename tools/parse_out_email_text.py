7eb1ec26bc4fe3517215866160e0228f734a0b43
anandvimal <colirsweb@gmail.com>
2018-03-18 10:28:09 -0500

    in text leanring chapter (before solving lesson 19). also added a folder nltk for example code.

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -8,12 +8,12 @@ def parseOutText(f):
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
@@ -30,17 +30,34 @@ def parseOutText(f):
         ### project part 2: comment out the line below
         words = text_string
 
+
+
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        stemmer = SnowballStemmer("english")
+        words = words.replace("  ", " ")
+        data_words = words.split(" ")
+
+        stemed_data = []
+
+        for i in data_words:
+            #print i
+            stemed_data.append(stemmer.stem(i))
+        #print ("words:\n",words)
+        #print stemed_data
+
+        #for i in stemed_data:
+            #print i
+
+        new_data_line =  ' '.join(stemed_data)
+        new_data_line = new_data_line.strip()
+
+        #print "new data line\n",new_data_line
+
+    return new_data_line
 
 
-
-
-    return words
-
-    
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
@@ -51,4 +68,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
