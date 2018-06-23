ba8d9d115d1c00c9b35c5fba7c36f7744f2f2d13
Shunya <colirsweb@gmail.com>
2018-06-22 23:10:00 -0500

    deleted all old files in repo.

--- nltk/snowball.py
+++
@@ -1,9 +0,0 @@
-from nltk.stem.snowball import SnowballStemmer
-stemmer = SnowballStemmer("english")
-stemmer.stem("responsiveness")
-
-stemmer.stem("responsivity")
-
-stemmer.stem("unresponsive")
-
-#
