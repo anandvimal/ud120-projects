d2bf278e242d562d485cc01b7661769a7d300cbe
Shunya <colirsweb@gmail.com>
2018-06-29 18:31:54 -0500

    decision tree ch4 Q37 solved
    decision tree impletemented with min number of split = 40
    accuract calculated was: 0.9778156996587031
    *accuracy varies little bit on each run.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -25,7 +25,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+#37 making decision tree classifier run with
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+#create decision tree classifier with min_samples_split = 40
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+
+y_pred = clf.predict(features_test)
+
+#accuracy_score(y_true, y_pred)
+accuracy = accuracy_score(labels_test, y_pred)
+
+print("accuracy at min_samples_split:40 with decision tree is",accuracy)
 #########################################################
 
+'''
+accuracy is 0.9778156996587031 with dt when min_samples_split = 40.
 
+'''
