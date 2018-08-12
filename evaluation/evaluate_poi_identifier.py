7d9dec7ef061cf4091130d4842e4b046510892c3
Shunya <colirsweb@gmail.com>
2018-08-12 18:12:21 -0500

    added code from ch14 to ch15 in evaluation folder.
    added code from ch 14 to ch 15 to start from. quiz27 chapter 15

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -26,6 +26,17 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### your code goes here 
+### your code goes here
+### it's all yours from here forward!
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
+X_train, X_test, y_train, y_test = train_test_split( features, labels, test_size=0.3, random_state=42)
 
+clf = DecisionTreeClassifier(random_state=0)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("accuracy on test dataset is: ", accuracy)
